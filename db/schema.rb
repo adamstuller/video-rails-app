a5bf24b05d5669fbe3590158129efd04221673e4
Adam Stuller <adam.stuller@protonmail.com>
2019-04-14 18:02:00 +0200

    user table, controller and model created

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_30_093803) do
+ActiveRecord::Schema.define(version: 2019_04_14_153410) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,11 @@ ActiveRecord::Schema.define(version: 2019_03_30_093803) do
     t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
   end
 
   create_table "videos", force: :cascade do |t|
